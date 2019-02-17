89eef4aa1501595324914846aa03cf16a3b5c938
zCoder <40376705+zchambers3@users.noreply.github.com>
2019-02-17 16:39:21 -0700

    Create Main.java

---
+++ Week1/CLC - Mini Project 1/Code/Example 1/Main.java
@@ -0,0 +1,24 @@
+package clcAssignment1;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		readWriteExample();
+		
+	}
+	private static void readWriteExample(){
+		fileReader reader = new fileReader("src\\Beans.txt");
+		reader.printList();
+		System.out.println("Adding new Bean...");
+		
+		ArrayList<exampleBeanOne> tempList = reader.getMyBeans();
+		exampleBeanOne bean = new exampleBeanOne("HotBean", 2, 333, "Chilly");
+		tempList.add(bean);
+		
+		reader.writeFile("src\\Beans.txt");
+		reader.printList();
+	}
+
+}
