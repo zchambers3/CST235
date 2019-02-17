e1e0dc1e59afb7c45adb922ed6073cdec47a2419
zCoder <40376705+zchambers3@users.noreply.github.com>
2019-02-17 16:40:10 -0700

    Create exampleBeanOne.java

---
+++ Week1/CLC - Mini Project 1/Code/Example 1/exampleBeanOne.java
@@ -0,0 +1,55 @@
+package clcAssignment1;
+
+import java.io.Serializable;
+
+public class exampleBeanOne implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String beanName;
+	private int beanID;
+	private int beanNum;
+	private String beanWord;
+	
+	public exampleBeanOne(){
+		beanName = null;
+		beanID = 0;
+		beanNum = 0;
+		beanWord = null;
+	}
+	public exampleBeanOne(String name, int id, int num, String word){
+		beanName = name;
+		beanID = id;
+		beanNum = num;
+		beanWord = word;
+	}
+	
+	public void setBeanName (String name){
+		this.beanName = name;
+	}
+	public String getBeanName (){
+		return beanName;
+	}
+	
+	public void setBeanID (int id){
+		this.beanID = id;
+	}
+	public int getBeanID (){
+		return beanID;
+	}
+	
+	public void setBeanNum (int num){
+		this.beanNum = num;
+	}
+	public int getBeanNum (){
+		return beanNum;
+	}
+	
+	public void setBeanWord (String word){
+		this.beanWord = word;
+	}
+	public String getBeanWord (){
+		return beanWord;
+	}
+	
+}
